d2c17a23ec7c2cd16b94ce0fa60eb94a23aee858
jiojio150 <jio060801@gmail.com>
2025-08-14 19:58:35 +0900

    Add files via upload

---
+++ ItemBackUp.java
@@ -0,0 +1,36 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해주세요.") // 기능이 제한되어있어 사용을 권장하지 않음
+public class ItemBackUp {
+
+    @NotNull(groups = UpdateCheck.class)
+    private Long id;
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class})
+    private Integer quantity;
+
+    public ItemBackUp() {
+    }
+
+    public ItemBackUp(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ ItemValidator.java
@@ -0,0 +1,50 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Slf4j
+@Component
+public class ItemValidator implements Validator {
+    /**
+     * Validator 이 여러개 일때 구분해주는 역할 해줌
+     * @param clazz
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Item.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Item item = (Item) target;
+
+        // 아래 대체 가능 (편리 기능, 간단한 기능만 제공)
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemName","required");
+
+        // 검증 로직
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.rejectValue("itemName", "required");
+//        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.rejectValue("price", "range", new Object[]{1000, 10000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+    }
+}
